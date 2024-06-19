8a88898717b4ae5f37dad1edf46593f0bfe53a66
Uglypr1nces <shpatavdiu4@gmail.com>
2024-06-19 09:20:24 +0200

    Add enemy characters and platforms

---
+++ game/scripts/platforms.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var platform1 = document.querySelector('.Platform1');
+    var platform2 = document.querySelector('.Platform2');
+    var platform3 = document.querySelector('.Platform3');
+});
